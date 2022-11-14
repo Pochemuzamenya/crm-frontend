ffa0bdd4ef033f8ecafabd3f2bd577dd6c598fd0
Andrey <filatovprivate@gmail.com>
2022-11-14 17:21:05 +0800

    feat: add update client

--- src/components/ClientCard.js
+++ src/components/ClientCard.js
@@ -1,9 +1,14 @@
 import {
     Box, List, ListItem,
     ListItemText, Divider,
-    ListItemIcon
+    ListItemIcon,
+    Button,
+    Dialog,
+    DialogTitle,
+    TextField,
+    Alert
 } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
 import BadgeRoundedIcon from '@mui/icons-material/BadgeRounded';
 import PhoneAndroidRoundedIcon from '@mui/icons-material/PhoneAndroidRounded';
@@ -12,11 +17,63 @@ import LocationCityRoundedIcon from '@mui/icons-material/LocationCityRounded';
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import AccountBalanceRoundedIcon from '@mui/icons-material/AccountBalanceRounded';
 import CommentBankRoundedIcon from '@mui/icons-material/CommentBankRounded';
+import { useUpdateClientMutation } from '../redux/api/clientsApi';
+import { useSelector } from 'react-redux';
+import { isEqual } from 'lodash'
 
 function ClientCard({ client }) {
+    const [open, setOpen] = useState(false)
+
+    const [updateClient, { isError, isSuccess }] = useUpdateClientMutation()
+
+    const manager = useSelector(state => state.user.currentUser.manager)
+
+    const [id, setId] = useState(client.id)
+    const [title, setTitle] = useState(client.title)
+    const [taxIdentificationNumber, setTaxIdentificationNumber] = useState(client.taxIdentificationNumber)
+    const [region, setRegion] = useState(client.region)
+    const [phoneNumber, setPhoneNumber] = useState(client.phoneNumber)
+    const [email, setEmail] = useState(client.email)
+    const [contactName, setContactName] = useState(client.contactName)
+    const [comment, setComment] = useState(client.comment)
+    const [clientAddress, setClientAddress] = useState(client.clientAddress)
+    const [city, setCity] = useState(client.city)
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const submitData = {
+        id, title, taxIdentificationNumber,
+        region, phoneNumber, email,
+        contactName, comment, clientAddress, city, manager
+    }
+
+    async function update() {
+        if (!isEqual(client, submitData)) {
+            try {
+                await updateClient(submitData).unwrap().then(console.log('success'))
+            } catch (error) {
+                console.log(`rejected with ${error}`)
+            }
+            setTitle(client.title)
+            setTaxIdentificationNumber(client.taxIdentificationNumber)
+            setRegion(client.region)
+            setPhoneNumber(client.phoneNumber)
+            setEmail(client.email)
+            setContactName(client.contactName)
+            setComment(client.comment)
+            setClientAddress(client.clientAddress)
+            setCity(client.city)
+        }
+    }
 
     return (
-        <Box sx={{flexGrow: 0.2,}}>
+        <Box sx={{ flexGrow: 0.2, }}>
             <List>
                 <ListItem button>
                     <ListItemIcon>
@@ -74,6 +131,30 @@ function ClientCard({ client }) {
                     <ListItemText primary={client.comment} secondary="Комментарий" />
                 </ListItem>
             </List>
+            <Button variant="outlined" onClick={handleClickOpen}>
+                Редактировать
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>{client.title}</DialogTitle>
+                <TextField label='Название' variant="filled" onChange={(e) => setTitle(e.target.value)} value={title} />
+                <TextField label='ИНН' variant="filled" onChange={(e) => setTaxIdentificationNumber(e.target.value)} value={taxIdentificationNumber} />
+                <TextField label='Имя контакта' variant="filled" onChange={(e) => setContactName(e.target.value)} value={contactName} />
+                <TextField label='Email' variant="filled" onChange={(e) => setEmail(e.target.value)} value={email} />
+                <TextField label='Телефон' variant="filled" onChange={(e) => setPhoneNumber(e.target.value)} value={phoneNumber} />
+                <TextField label='Регион' variant="filled" onChange={(e) => setRegion(e.target.value)} value={region} />
+                <TextField label='Адрес' variant="filled" onChange={(e) => setClientAddress(e.target.value)} value={clientAddress} />
+                <TextField label='Город' variant="filled" onChange={(e) => setCity(e.target.value)} value={city} />
+                <TextField label='Комментарий' variant="filled" onChange={(e) => setComment(e.target.value)} value={comment} />
+                <Button onClick={() => update()} >
+                    Сохранить
+                </Button>
+                {isSuccess ? <Alert variant="filled" severity="success">
+                    This is a success alert — check it out!
+                </Alert> : <Alert variant="filled" severity="error">
+                    This is an error alert — check it out!
+                </Alert>}
+            </Dialog>
+
         </Box>
     )
 }
