46d1bd49eb0268793b9020eab94b99ed1384e0b4
Pochemuzamenya <filatovprivate@gmail.com>
2022-12-09 16:24:02 +0800

    feat: add feature for add client from API to BD

--- src/components/GisFetchDataGrid.js
+++ src/components/GisFetchDataGrid.js
@@ -1,7 +1,11 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Button, Typography } from '@mui/material'
 import { DataGrid } from '@mui/x-data-grid'
 import LinearProgress from '@mui/material/LinearProgress';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import GisList from '../components/GisList'
+import { useSelector } from 'react-redux';
+import { useAddClientMutation } from '../redux/api/clientsApi'
+import { useLazyFetchCityByNameQuery, useLazyFetchPlaceByCityQuery } from '../redux/api/gisApi'
 
 
 const columns = [
@@ -12,7 +16,44 @@ const columns = [
 
 function GisFetchDataGrid({ data }) {
 
+  const manager = useSelector(state => state.user.currentUser.manager)
+  const [client, setClient] = useState({})
 
+  const [addClient, { isError }] = useAddClientMutation()
+
+  const [title, setTitle] = useState('')
+  const [clientAddress, setClientAddress] = useState('')
+
+  const [taxIdentificationNumber, setTaxIdentificationNumber] = useState('')
+  const [region, setRegion] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [email, setEmail] = useState('')
+  const [contactName, setContactName] = useState('')
+  const [comment, setComment] = useState('')
+  const [city, setCity] = useState('')
+
+  const submitData = {
+    title: client.name,
+    clientAddress: client.address_name,
+    manager,
+    taxIdentificationNumber,
+    region,
+    phoneNumber,
+    email,
+    contactName,
+    comment,
+    city,
+  }
+
+  const handleClick = async () => {
+    try {
+      await addClient(submitData).unwrap()
+    } catch (error) {
+      console.log(`rejected with ${error}`)
+    }
+  }
+
+  console.log(client)
   return (
     <Box sx={{ height: 400, width: '100%' }}>
       <DataGrid
@@ -23,9 +64,16 @@ function GisFetchDataGrid({ data }) {
         autoPageSize
         experimentalFeatures={{ newEditingApi: true }}
         components={{
- 
+
+        }}
+        onRowClick={(data) => {
+          setClient(data.row)
         }}
       />
+      <Button
+        variant='contained'
+        onClick={() => handleClick()}
+      >send to base</Button>
     </Box>
   )
 }
